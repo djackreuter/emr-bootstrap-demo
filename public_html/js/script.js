fa63e0b14a1d33adbce177c90a8218cb15279185
Jack Reuter <djreuter45@gmail.com>
2017-12-21 14:58:53 -0700

    added location with google maps js api

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,3 +3,19 @@ $(function() {
     target: '#navbar'
   });
 });
+
+
+  function initMap() {
+    var emrBear = {
+      lat: 35.675110,
+      lng: -105.962104
+    };
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 14,
+      center: emrBear
+    });
+    var marker = new google.maps.Marker({
+      position: emrBear,
+      map: map
+    });
+  }
