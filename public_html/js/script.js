c6ae9f4dc6f25bdb9f3cb021e3abe1a8f03cc3db
Jack Reuter <djreuter45@gmail.com>
2017-12-22 11:54:26 -0700

    added place info to Google map

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,21 +1,36 @@
 $(function() {
-  $('body').scrollspy({
-    target: '#navbar'
-  });
+    $('body').scrollspy({
+        target: '#navbar'
+    });
 });
 
 
-  function initMap() {
+function initMap() {
     var emrBear = {
-      lat: 35.675110,
-      lng: -105.962104
+        lat: 35.675110,
+        lng: -105.962104
     };
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 14,
-      center: emrBear
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: emrBear
     });
-    var marker = new google.maps.Marker({
-      position: emrBear,
-      map: map
+
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails({
+        placeId: 'ChIJoRoGb3pQGIcRFp_otOPZtE0'
+    }, function(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                    place.formatted_address + '</div>');
+                infowindow.open(map, this);
+            });
+        }
     });
-  }
+}
